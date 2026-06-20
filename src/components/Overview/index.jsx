41a86e82eaeea3701f3a439783b871ff284a9c56
bhanuprakash47 <bhanuprakashnampally54@gmail.com>
2026-06-20 11:08:14 +0530

    Add css to Overview Section

--- src/components/Overview/index.jsx
+++ src/components/Overview/index.jsx
@@ -3,6 +3,7 @@ import { LuDollarSign, LuArrowLeftRight } from "react-icons/lu"
 import { PiCurrencyCircleDollarFill } from "react-icons/pi"
 import { BsCreditCardFill } from "react-icons/bs"
 
+import "./index.css"
 
 const Overview=(props)=>{
     const {overviewDetails}=props 
@@ -20,18 +21,20 @@ const Overview=(props)=>{
     return(
         <div className="dashboard-section-container">
             <h1 className="dashboard-section-heading">Overview</h1>
-            {overviewDetails.map((each,index) => {
-                const Icon=iconsList[index]
-                return(
-                    <div key={each.id} className="overview-element">
-                        <div className="icon-container">
-                            {Icon&&<Icon size={28}/>}
-                        </div>
-                        <h1 className="overview-label">{index}{each.label}</h1>
-                        <p className="overview-label-value">{each.value}</p>
-                    </div>
-                )
-            })}
+            <ul className="list">
+                {overviewDetails.map((each,index) => {
+                    const Icon=iconsList[index]
+                    return(
+                        <li key={each.id} className="overview-element">
+                            <div className="icon-container">
+                                {Icon&&<Icon size={21}/>}
+                            </div>
+                            <h1 className="overview-label-value">{each.value}</h1>
+                            <p className="overview-label">{each.label}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
